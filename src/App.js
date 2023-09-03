8593f51cbb98042e803ba5f7220b02b0b2cc087e
UmaM03 <umam11032000@gmail.com>
2023-09-03 19:35:26 +0530

    practiced

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import Header from './Header';
-import Name from './Name';
-
+import Gallery from './Profile';
+// import Wish from './Wish.js';
+// import Welcome from './Welcome.js';
 
 
 function App() {
   return (
     <div className="App">
-      <h1> Hey  hi Everyone,welcome todetown</h1>
-      <Header/>
-      <Name/>
+      {/* <h1> Hey  hi Everyone,welcome todetown</h1>
+      
+      <Wish name="James" hobby="cricket">
+        <p>this side is Uma !!!</p>
+        <button>Click here</button>
+      </Wish>
+      <Wish name="John" hobby="cricket"/>
+      <Wish name="Clark" hobby="cricket"/>
 
+      <Welcome name="Uma " hobby=" coding"></Welcome> */}
+
+      <Gallery />
+ 
     </div>
    
   
--- src/wishes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
- const wishes=()=>{
-    <div>
-        <h1> Anshika gupta</h1>
-    </div>
- }
-
-export default wishes
